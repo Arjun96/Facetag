0d0863d0a26f63b712ce085e15ebf38150b8695e
Arjun96 <luthra.arjun@gmail.com>
2016-03-12 16:02:19 -0500

    More button functionality

--- app/src/main/java/com/example/arjun/nhacks/ListFacesActivity.java
+++ app/src/main/java/com/example/arjun/nhacks/ListFacesActivity.java
@@ -19,7 +19,7 @@ public class ListFacesActivity extends AppCompatActivity {
     }
 
     public void exit(View view){
-        Intent intent = new Intent(this,MainActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
