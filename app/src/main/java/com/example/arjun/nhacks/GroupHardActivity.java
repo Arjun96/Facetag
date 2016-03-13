121b80ecc9a4d787ee5e4d361ee6a1e4a152b347
trickly <patrick.lee.96@gmail.com>
2016-03-12 19:50:46 -0500

    Themeing

--- app/src/main/java/com/example/arjun/nhacks/GroupHardActivity.java
+++ app/src/main/java/com/example/arjun/nhacks/GroupHardActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class GroupHardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
